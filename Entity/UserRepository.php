d69d005ec5b146891920d4efc7d79163009498be
Fahmi Laajimi <fahmi.laajimi@canaltp.fr>
2020-03-19 17:48:05 +0100

    Remove useless getIncativeUsersSince

--- Entity/UserRepository.php
+++ Entity/UserRepository.php
@@ -6,13 +6,5 @@ use Doctrine\ORM\EntityRepository;
 
 class UserRepository extends EntityRepository
 {
-    public function getIncativeUsersSince(\DateTime $lastLoginDate)
-    {
-        $qb = $this->createQueryBuilder('u')
-            ->where('u.lastLogin < :lastLoginDate')
-            ->orWhere('u.lastLogin IS NULL')
-            ->setParameter('lastLoginDate', $lastLoginDate);
 
-        return $qb->getQuery()->getResult();
-    }
 }
